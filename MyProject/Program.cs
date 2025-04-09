af8e22d2f8e3bb2b3d201d1657da489e3e154508
Gokul322003 <161920476+Gokul322003@users.noreply.github.com>
2025-04-09 07:15:35 +0000

    hi

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace cproject{
+    class Program{
+        static void Main (String [] args){
+
+            Console.WriteLine("Hello World");
+            // static datatypes
+            int a= 5;
+            string name = "tony";
+            double b= 34.0;
+            Console.WriteLine(a + name);
+
+            // dynamic datatypes
+            var age  =55;                                      
+            //  in this it will automatically get the data type at runtime
+            dynamic hieght = 76;
+            // again i can assign value to the same varialbe hieght because it is dynamic datatype
+            hieght = 100;
+            Console.WriteLine(hieght);
+
+
+
+        }
+    }
+}
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bdeb3dfdef04aab0b63a04ec4f1d47f36b41110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf483c09be27a66dbd72979733ac525a3e12540a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
